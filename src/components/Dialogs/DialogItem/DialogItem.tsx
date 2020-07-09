1861a703ac67f633b8cb08523f84f5cbb08dbfa5
Karter13 <karmih1975@mail.ru>
2020-07-09 17:16:47 +0300

    added uuid

--- src/components/Dialogs/DialogItem/DialogItem.tsx
+++ src/components/Dialogs/DialogItem/DialogItem.tsx
@@ -3,7 +3,7 @@ import s from './../Dialogs.module.css'
 import {NavLink} from "react-router-dom";
 
 type DialogPropsType = {
-    id: number
+    id: string
     name: string
     img: string
 }
--- src/redux/state.tsx
+++ src/redux/state.tsx
@@ -1,19 +1,21 @@
+import {v1} from "uuid";
+
 export type PostType = {
-    id: number
+    id: string
     message: string
     likesCount: number
 }
 export type DialogType = {
-    id: number
+    id: string
     name: string
     img: string
 }
 export type MessageType = {
-    id: number
+    id: string
     message: string
 }
 export type FriendSidebarType = {
-    id: number
+    id: string
     name: string
     img: string
 }
@@ -36,30 +38,30 @@ export type RootStateType = {
 export let state: RootStateType = {
     profilePage: {
         posts: [
-            {id: 1, message: 'Good post', likesCount: 15},
-            {id: 2, message: 'I love React', likesCount: 20},
-            {id: 3, message: 'I love JS', likesCount: 10},
+            {id: v1(), message: 'Good post', likesCount: 15},
+            {id: v1(), message: 'I love React', likesCount: 20},
+            {id: v1(), message: 'I love JS', likesCount: 10},
         ]
     },
     dialogsPage: {
         dialogs: [
-            {id: 1, name: 'Maikl', img: "http://avatars.mds.yandex.net/get-pdb/1245924/bc43f857-5d36-4e10-a9e1-4f838cbb5753/s1200?webp=false"},
-            {id: 2, name: 'Anna', img: "http://avatars.mds.yandex.net/get-pdb/1245924/bc43f857-5d36-4e10-a9e1-4f838cbb5753/s1200?webp=false"},
-            {id: 3, name: 'Masha', img: "http://avatars.mds.yandex.net/get-pdb/1245924/bc43f857-5d36-4e10-a9e1-4f838cbb5753/s1200?webp=false"},
-            {id: 4, name: 'Aleksey', img: "http://avatars.mds.yandex.net/get-pdb/1245924/bc43f857-5d36-4e10-a9e1-4f838cbb5753/s1200?webp=false"},
+            {id: v1(), name: 'Maikl', img: "http://avatars.mds.yandex.net/get-pdb/1245924/bc43f857-5d36-4e10-a9e1-4f838cbb5753/s1200?webp=false"},
+            {id: v1(), name: 'Anna', img: "http://avatars.mds.yandex.net/get-pdb/1245924/bc43f857-5d36-4e10-a9e1-4f838cbb5753/s1200?webp=false"},
+            {id: v1(), name: 'Masha', img: "http://avatars.mds.yandex.net/get-pdb/1245924/bc43f857-5d36-4e10-a9e1-4f838cbb5753/s1200?webp=false"},
+            {id: v1(), name: 'Aleksey', img: "http://avatars.mds.yandex.net/get-pdb/1245924/bc43f857-5d36-4e10-a9e1-4f838cbb5753/s1200?webp=false"},
         ],
         messages: [
-            {id: 1, message: 'Hello my friend'},
-            {id: 1, message: 'Good morning'},
-            {id: 1, message: 'Good by'},
-            {id: 1, message: 'Yo'},
+            {id: v1(), message: 'Hello my friend'},
+            {id: v1(), message: 'Good morning'},
+            {id: v1(), message: 'Good by'},
+            {id: v1(), message: 'Yo'},
         ]
     },
     sidebar: {
         friends: [
-            {id: 1, name: 'Friends1', img: "http://avatars.mds.yandex.net/get-pdb/1245924/bc43f857-5d36-4e10-a9e1-4f838cbb5753/s1200?webp=false"},
-            {id: 2, name: 'Friends2', img: "http://avatars.mds.yandex.net/get-pdb/1245924/bc43f857-5d36-4e10-a9e1-4f838cbb5753/s1200?webp=false"},
-            {id: 3, name: 'Friends3', img: "http://avatars.mds.yandex.net/get-pdb/1245924/bc43f857-5d36-4e10-a9e1-4f838cbb5753/s1200?webp=false"},
+            {id: v1(), name: 'Friends1', img: "http://avatars.mds.yandex.net/get-pdb/1245924/bc43f857-5d36-4e10-a9e1-4f838cbb5753/s1200?webp=false"},
+            {id: v1(), name: 'Friends2', img: "http://avatars.mds.yandex.net/get-pdb/1245924/bc43f857-5d36-4e10-a9e1-4f838cbb5753/s1200?webp=false"},
+            {id: v1(), name: 'Friends3', img: "http://avatars.mds.yandex.net/get-pdb/1245924/bc43f857-5d36-4e10-a9e1-4f838cbb5753/s1200?webp=false"},
         ]
     }
 
