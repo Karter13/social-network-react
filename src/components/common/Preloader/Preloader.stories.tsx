7b3aa76dab840cb6c948f7f944c77bebbd72715a
Karter13 <KarMiH1975@mail.ru>
2020-09-12 21:54:53 +0300

    installed storybook addon-storysource and created Preloader.stories.tsx

---
+++ src/components/common/Preloader/Preloader.stories.tsx
@@ -0,0 +1,11 @@
+import {Preloader} from './Preloader';
+import React from 'react';
+
+export default {
+    title: 'Preloader component',
+    component: Preloader,
+}
+
+export const PreloaderBaseExample = () => {
+    return <Preloader/>
+};
