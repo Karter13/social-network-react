e69bf32a0a749e774b5d58e83e59283b465806c4
Karter13 <KarMiH1975@mail.ru>
2020-09-12 20:45:36 +0300

    created test for auth-reducer

---
+++ src/redux/auth-reducer.test.tsx
@@ -0,0 +1,23 @@
+import {authReducer, AuthType, setAuthUserData} from './auth-reducer';
+
+test('correct get users data', () => {
+    const startState: AuthType = {
+        userId: null,
+        email: null,
+        login: null,
+        isAuth: false
+    };
+
+    const userId = 10;
+    const email = 'karamzin@mail.ru';
+    const login = '12121212';
+
+
+    const action = setAuthUserData(userId, email, login);
+    const endState = authReducer(startState, action);
+
+    expect(endState.isAuth).toBeTruthy();
+    expect(endState.userId).toBe(10);
+    expect(endState.email).toBe('karamzin@mail.ru');
+    expect(endState.login).toBe('12121212');
+});
