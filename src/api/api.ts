ef8fd18b0b997015eb46ec511f65a91a142720e9
Karter13 <KarMiH1975@mail.ru>
2020-09-14 22:39:18 +0300

    created Generic types API for reducer thunks

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,5 +1,42 @@
 import axios from 'axios';
-import {LocationType, PhotosType} from '../redux/users-reducer';
+import {PhotosType} from '../redux/users-reducer';
+import {ProfileType} from '../redux/profile-reducer';
+
+type UserType = {
+    id: string
+    photos: PhotosType
+    followed: boolean
+    name: string
+    status: string
+}
+type AuthType = {
+    id: number | null
+    email: string | null
+    login: string | null
+}
+type APIUsersType = {
+    items: Array<UserType>
+    totalCount: number
+    error: null | string
+}
+
+type CommonResponseType<T> = {
+    resultCode: 1 | 0
+    messages: Array<string>
+    data: T
+}
+
+// type APIFollowType = {
+//     resultCode: 1 | 0
+//     messages: Array<string>
+//     data: {}
+// }
+//
+// type APIAuthPI = {
+//     resultCode: 1 | 0
+//     messages: Array<string>
+//     data: AuthType
+// }
 
 const instance = axios.create({
     withCredentials: true,
@@ -9,50 +46,28 @@ const instance = axios.create({
     },
 });
 
-
-type UserType = {
-    id: string
-    photos: PhotosType
-    followed: boolean
-    name: string
-    status: string
-}
-type APIUsersType = {
-    items: Array<UserType>
-    totalCount: number
-    error: null | string
-}
-
-type ARIFollowType = {
-    resultCode: 1 | 0
-    messages: Array<string>
-    data: {}
-}
-
-
-
 export const usersAPI = {
     getUsers(currentPage: number = 1, pageSize: number = 10) {
         return instance.get<APIUsersType>(`users?page=${currentPage}&count=${pageSize}`)
             .then(response => response.data);
     },
     follow(id: string) {
-        return instance.post<ARIFollowType>(`follow/${id}`)
+        return instance.post<CommonResponseType<{}>>(`follow/${id}`)
             .then(response => response.data)
     },
     unfollow(id: string) {
-        return instance.delete<ARIFollowType>(`follow/${id}`)
+        return instance.delete<CommonResponseType<{}>>(`follow/${id}`)
             .then(response => response.data)
     },
     getProfile(userId: string) {
-        return instance.get('profile/' + userId)
+        return instance.get<ProfileType>('profile/' + userId)
             .then(response => response.data)
     },
 };
 
 export const authPI = {
     me() {
-        return instance.get(`auth/me`)
+        return instance.get<CommonResponseType<AuthType>>(`auth/me`)
             .then(response => response.data)
     },
 };
