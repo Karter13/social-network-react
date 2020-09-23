bbe177e04873362286726b76bc3db602c6734dd6
Karter13 <karmih1975@mail.ru>
2020-09-23 15:43:12 +0300

    created LoginForm and installed redux-form

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -2,7 +2,33 @@ import React from 'react';
 import style from './Login.module.css'
 
 export type LoginPropsType = {}
+export type LoginFormPropsType = {}
+
+export const LoginForm: React.FC<LoginFormPropsType> = (props) => {
+    return (
+            <form action="">
+                <div>
+                    <input placeholder={'Login'}/>
+                </div>
+                <div>
+                    <input placeholder={'Password'}/>
+                </div>
+                <div>
+                    <input type={'checkbox'}/>remember me
+                </div>
+                <div>
+                    <button>Login</button>
+                </div>
+
+            </form>
+    )
+};
 
 export const Login: React.FC<LoginPropsType> = (props) => {
-    return <h1 >LOGIN</h1>
+    return (
+        <div>
+            <h1 >LOGIN</h1>
+            <LoginForm/>
+        </div>
+    )
 };
