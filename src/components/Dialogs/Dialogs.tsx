165a6e563f2ad2c14ac36f18eab278f6b5c1f90e
Karter13 <karmih1975@mail.ru>
2020-09-28 16:16:57 +0300

    validate textarea into Dialogs

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -4,6 +4,8 @@ import {Message} from './Message/Message';
 import {DialogItem} from './DialogItem/DialogItem';
 import {DialogsPageType} from '../../redux/dialogs-reducer';
 import {Field, InjectedFormProps, reduxForm} from 'redux-form';
+import {Textarea} from '../common/FormControls/FormControls';
+import {maxLengthCreator, required} from '../../utils/validators/validators';
 
 export type DialogsPropsType = {
     dialogsPage: DialogsPageType
@@ -44,11 +46,16 @@ type AddMessagesFormType = {
     newMessageBody: string
 }
 
+const maxLength100 = maxLengthCreator(100);
+
 export const AddMessageForm: React.FC<InjectedFormProps<AddMessagesFormType>> = (props) => {
     return (
         <form onSubmit={props.handleSubmit}>
             <div>
-                <Field component={'textarea'} name={'newMessageBody'} placeholder={'Enter your message'} />
+                <Field component={Textarea}
+                       validate={[required, maxLength100]}
+                       name={'newMessageBody'}
+                       placeholder={'Enter your message'} />
             </div>
             <div>
                 <button>add message</button>
