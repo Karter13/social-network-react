57139671d7e1dd3b29d03272c162a6254ef0bfd8
Karter13 <KarMiH1975@mail.ru>
2020-10-03 15:51:54 +0300

    made App a class component from a functional component

--- src/App.tsx
+++ src/App.tsx
@@ -4,30 +4,53 @@ import {Navbar} from './components/Navbar/Navbar';
 import {Music} from './components/Music/Music';
 import {News} from './components/News/News';
 import {Settings} from './components/Settings/Settings';
-import {Route} from 'react-router-dom';
+import {Route, withRouter} from 'react-router-dom';
 import {DialogsContainer} from './components/Dialogs/DialogsContainer';
 import {UsersContainer} from './components/Users/UsersContainer';
 import ProfileContainer from './components/Profile/ProfileContainer';
 import HeaderContainer from './components/Header/HeaderContainer';
 import Login from './components/Login/Login';
+import {connect} from 'react-redux';
+import {StateType} from './redux/redux-store';
+import {getAuthUserData} from './redux/auth-reducer';
+import {compose} from 'redux';
 
-type AppPropsType = {}
-export const App: React.FC<AppPropsType> = (props) => {
-    return (
-        <div className='app-wrapper'>
-            <HeaderContainer/>
-            <Navbar/>
-            <div className='app-wrapper-content'>
-                <Route path='/profile/:userId?' render={() => <ProfileContainer />}/>
-                <Route path='/dialogs' render={() => <DialogsContainer/>}/>
-                <Route path='/news' render={() => <News/>}/>
-                <Route path='/music' render={() => <Music/>}/>
-                <Route path='/settings' render={() => <Settings/>}/>
-                <Route path='/users' render={() => <UsersContainer pageTitle={'Самураи'}/>}/>
-                <Route path='/login' render={() => <Login/>}/>
+export type OwnPropsType = {}
+export type MapStatePropsType = {}
+export type MapDispatchPropsType = {
+    getAuthUserData: () => void
+}
+
+type AppPropsType = OwnPropsType & MapStatePropsType & MapDispatchPropsType
+
+class App extends React.Component<AppPropsType> {
+
+    componentDidMount(): void {
+        this.props.getAuthUserData();
+    }
+
+    render() {
+        return (
+            <div className='app-wrapper'>
+                <HeaderContainer/>
+                <Navbar/>
+                <div className='app-wrapper-content'>
+                    <Route path='/profile/:userId?' render={() => <ProfileContainer/>}/>
+                    <Route path='/dialogs' render={() => <DialogsContainer/>}/>
+                    <Route path='/news' render={() => <News/>}/>
+                    <Route path='/music' render={() => <Music/>}/>
+                    <Route path='/settings' render={() => <Settings/>}/>
+                    <Route path='/users' render={() => <UsersContainer pageTitle={'Самураи'}/>}/>
+                    <Route path='/login' render={() => <Login/>}/>
+                </div>
             </div>
-        </div>
-    );
-};
+        );
+    }
+}
 
-
+//правильная ли типизация  compose
+export default compose<React.ComponentClass>(
+    // withRouter,
+    connect<MapStatePropsType, MapDispatchPropsType, OwnPropsType, StateType>
+    (null, {getAuthUserData}))
+(App)
--- src/components/Header/HeaderContainer.tsx
+++ src/components/Header/HeaderContainer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {Header} from './Header';
 import {connect} from 'react-redux';
 import {StateType} from '../../redux/redux-store';
-import {getAuthUserData, logout} from '../../redux/auth-reducer';
+import {logout} from '../../redux/auth-reducer';
 
 type OwnPropsType = {}
 type MapStatePropsType = {
@@ -10,16 +10,11 @@ type MapStatePropsType = {
     login: string | null
 }
 type MapDispatchPropsType = {
-    getAuthUserData: () => void
     logout: () => void
 }
 type HeaderContainerPropsType = MapStatePropsType & MapDispatchPropsType & OwnPropsType
 
 class HeaderContainer extends React.Component<HeaderContainerPropsType> {
-
-    componentDidMount(): void {
-        this.props.getAuthUserData();
-    }
 
     render() {
         return <Header {...this.props}/>
@@ -32,6 +27,6 @@ const mapStateToProps = (state: StateType) => {
         login: state.auth.login,
     }
 };
-export default connect<MapStatePropsType, MapDispatchPropsType, OwnPropsType, StateType>(mapStateToProps, {getAuthUserData, logout})(HeaderContainer)
+export default connect<MapStatePropsType, MapDispatchPropsType, OwnPropsType, StateType>(mapStateToProps, {logout})(HeaderContainer)
 
 
