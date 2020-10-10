0fafcd438f976e9b78f5b0ac8d1c0a48b3f04bb9
Karter13 <KarMiH1975@mail.ru>
2020-10-10 16:48:31 +0300

    added functions for ProfileStatusWithHooks

--- src/components/Profile/ProfileInfo/ProfileStatusWithHooks.tsx
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.tsx
@@ -1,28 +1,41 @@
-import React from 'react';
+import React, {ChangeEvent, useState} from 'react';
 
 type ProfileStatusType = {
     status: string
     updateStatus: (status: string) => void
 }
-type StateType = {
-    editMode: boolean,
-    status: string
-}
 
 export const ProfileStatusWithHooks: React.FC<ProfileStatusType> = (props) => {
 
+    let [editMode, setEditMode] = useState(false);
+    let [status, setStatus] = useState(props.status);
+
+    const activateEditMode = () => {
+        setEditMode(true);
+    };
+
+    const deActivateEditMode = () => {
+        setEditMode(false);
+        props.updateStatus(status)
+    };
+
+    const onStatusChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setStatus(e.currentTarget.value)
+    };
 
     return (
         <div>
-            {
+            {!editMode &&
                 <div>
-                    <span>{props.status || '--------'}</span>
+                    <span onDoubleClick={activateEditMode}>{props.status || '--------'}</span>
                 </div>
             }
-            {false &&
+            {editMode &&
             <div>
                 <input autoFocus={true}
-                       value={''}/>
+                       onChange={onStatusChange}
+                       onBlur={deActivateEditMode}
+                       value={status}/>
             </div>
             }
         </div>
