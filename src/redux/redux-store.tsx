cfa88215c4431913e70b9e3568a0bcb43bea170b
Karter13 <KarMiH1975@mail.ru>
2020-10-22 19:22:51 +0300

    installed Redux Dev Tools

--- src/redux/redux-store.tsx
+++ src/redux/redux-store.tsx
@@ -7,6 +7,7 @@ import {authReducer} from './auth-reducer';
 import thunkMiddleware from 'redux-thunk';
 import { reducer as formReducer } from 'redux-form'
 import {appReducer} from './app-reducer';
+import {composeWithDevTools} from 'redux-devtools-extension';
 
 
 let reducers = combineReducers({
@@ -21,7 +22,9 @@ let reducers = combineReducers({
 
 export type StateType = ReturnType<typeof reducers>
 
-export let store = createStore(reducers, applyMiddleware(thunkMiddleware));
+export let store = createStore(reducers, composeWithDevTools(applyMiddleware(thunkMiddleware)));
 
 // @ts-ignore
 window.store = store;
+
+
