b4cc355137ba590c27b01f49796e013acbeaad39
Karter13 <KarMiH1975@mail.ru>
2020-11-01 15:29:35 +0300

    Updates

---
+++ precache-manifest.74df117cc747f3d4f862f383d7766d55.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f3e7e9d33a33fd8fdc8615e883e198e",
+    "url": "/social-network-react/index.html"
+  },
+  {
+    "revision": "8f684cb9b6002faed362",
+    "url": "/social-network-react/static/css/3.dfb6d27f.chunk.css"
+  },
+  {
+    "revision": "bd34bd75fc9f6247c08c",
+    "url": "/social-network-react/static/css/4.d92b4378.chunk.css"
+  },
+  {
+    "revision": "d435542e6034f624e4e9",
+    "url": "/social-network-react/static/css/main.6314b01a.chunk.css"
+  },
+  {
+    "revision": "185df5b0513d0230dce5",
+    "url": "/social-network-react/static/js/2.97b09969.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/social-network-react/static/js/2.97b09969.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f684cb9b6002faed362",
+    "url": "/social-network-react/static/js/3.8984dd65.chunk.js"
+  },
+  {
+    "revision": "bd34bd75fc9f6247c08c",
+    "url": "/social-network-react/static/js/4.34dd6c63.chunk.js"
+  },
+  {
+    "revision": "d435542e6034f624e4e9",
+    "url": "/social-network-react/static/js/main.fe22f7c7.chunk.js"
+  },
+  {
+    "revision": "03dc44fac5a719c77151",
+    "url": "/social-network-react/static/js/runtime-main.b3769026.js"
+  },
+  {
+    "revision": "b31e49524aabcf9f6a17a7e59b59b2be",
+    "url": "/social-network-react/static/media/loader.b31e4952.svg"
+  }
+]);
