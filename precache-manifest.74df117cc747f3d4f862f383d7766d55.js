5559965ae20453bab1f874ba89a5e8bd5e400882
Karter13 <KarMiH1975@mail.ru>
2020-11-01 15:50:59 +0300

    Updates

--- precache-manifest.74df117cc747f3d4f862f383d7766d55.js
+++ precache-manifest.74df117cc747f3d4f862f383d7766d55.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c25549defe76afde913d7748e03c5a93",
+    "revision": "4f3e7e9d33a33fd8fdc8615e883e198e",
     "url": "/social-network-react/index.html"
   },
   {
@@ -8,36 +8,36 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/social-network-react/static/css/3.dfb6d27f.chunk.css"
   },
   {
-    "revision": "7dcd35fe6f48980aab16",
+    "revision": "bd34bd75fc9f6247c08c",
     "url": "/social-network-react/static/css/4.d92b4378.chunk.css"
   },
   {
-    "revision": "f79d38bb4f048a8af38f",
+    "revision": "d435542e6034f624e4e9",
     "url": "/social-network-react/static/css/main.6314b01a.chunk.css"
   },
   {
-    "revision": "6ee168b2cfedb45e8a0c",
-    "url": "/social-network-react/static/js/2.a19efa5e.chunk.js"
+    "revision": "185df5b0513d0230dce5",
+    "url": "/social-network-react/static/js/2.97b09969.chunk.js"
   },
   {
     "revision": "3453b8997016469371284a28c0e873e2",
-    "url": "/social-network-react/static/js/2.a19efa5e.chunk.js.LICENSE.txt"
+    "url": "/social-network-react/static/js/2.97b09969.chunk.js.LICENSE.txt"
   },
   {
     "revision": "8f684cb9b6002faed362",
     "url": "/social-network-react/static/js/3.8984dd65.chunk.js"
   },
   {
-    "revision": "7dcd35fe6f48980aab16",
-    "url": "/social-network-react/static/js/4.2e867d3d.chunk.js"
+    "revision": "bd34bd75fc9f6247c08c",
+    "url": "/social-network-react/static/js/4.34dd6c63.chunk.js"
   },
   {
-    "revision": "f79d38bb4f048a8af38f",
-    "url": "/social-network-react/static/js/main.bd0eb436.chunk.js"
+    "revision": "d435542e6034f624e4e9",
+    "url": "/social-network-react/static/js/main.fe22f7c7.chunk.js"
   },
   {
-    "revision": "5daf641c29416d350c46",
-    "url": "/social-network-react/static/js/runtime-main.e3878aef.js"
+    "revision": "03dc44fac5a719c77151",
+    "url": "/social-network-react/static/js/runtime-main.b3769026.js"
   },
   {
     "revision": "b31e49524aabcf9f6a17a7e59b59b2be",
