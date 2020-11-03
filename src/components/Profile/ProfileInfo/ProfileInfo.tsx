e54e0a77533186f849d8bb345cb92999991199d3
Karter13 <KarMiH1975@mail.ru>
2020-11-03 20:02:43 +0300

    created components ProfileDataForm and ProfileData

--- src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {ChangeEvent, useState} from 'react';
 import s from './ProfileInfo.module.css'
 import {Preloader} from '../../common/Preloader/Preloader';
 import {ContactsType, ProfileType} from '../../../redux/profile-reducer';
@@ -13,13 +13,15 @@ export type ProfileInfoPropsType = {
     savePhoto: any
 }
 export const ProfileInfo: React.FC<ProfileInfoPropsType> = ({profile, status, updateStatus, isOwner, savePhoto}) => {
+
+    let [editMode, setEditMode] = useState(false);
+
     if (!profile) {
         return <Preloader/>
     }
-    //types for e?
-    // DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
-    const onMainPhotoSelected = (e: any) => {
-        if (e.target.files.length) {
+
+    const onMainPhotoSelected = (e: ChangeEvent<HTMLInputElement>) => {
+        if (e.target.files) {
             savePhoto(e.target.files[0])
         }
     };
@@ -32,44 +34,82 @@ export const ProfileInfo: React.FC<ProfileInfoPropsType> = ({profile, status, up
                 </div>
                 {isOwner && <input type={'file'} onChange={onMainPhotoSelected}/>}
 
-                <div>
-                    <div>
-                        <b>FullName:</b>{profile.fullName}
-                    </div>
-                    <div>
-                        <b>LookingForAJob:</b>{profile.lookingForAJob ? 'YES' : 'No'}
-                    </div>
-                    {profile.lookingForAJob &&
-                    <div>
-                        <b>My professional skills:</b>{profile.lookingForAJobDescription}
-                    </div>
-                    }
-                    <div>
-                        <b>About me:</b>{profile.aboutMe ? 'YES' : 'No'}
-                    </div>
-                    <div>
-                        <b>Contacts</b>: {Object.keys(profile.contacts).map(key => {
-                           return <Contact key={key} contactTitle = {key} contactValue = {'profile.contacts[key]' }/>
-                    })}
-                    </div>
-                </div>
+                {editMode ? <ProfileDataForm profile={profile}/> : <ProfileData profile={profile}/>}
 
-                <ProfileStatusWithHooks status={status}
-                                        updateStatus={updateStatus}
-                />
+                <ProfileStatusWithHooks status={status} updateStatus={updateStatus}/>
             </div>
         </div>
     )
 };
 
+type ProfileDataPropsType = {
+    profile: ProfileType | null
+}
+const ProfileData: React.FC<ProfileDataPropsType> = ({profile}) => {
+    return (
+        profile && <div>
+            <div>
+                <b>FullName:</b>{profile.fullName}
+            </div>
+            <div>
+                <b>LookingForAJob:</b>{profile.lookingForAJob ? 'YES' : 'No'}
+            </div>
+            {profile.lookingForAJob &&
+            <div>
+                <b>My professional skills:</b>{profile.lookingForAJobDescription}
+            </div>
+            }
+            <div>
+                <b>About me:</b>{profile.aboutMe ? 'YES' : 'No'}
+            </div>
+            <div>
+                <b>Contacts</b>: {(Object.keys(profile.contacts) as Array<keyof ContactsType>).map(key => {
+                return <Contact key={key} contactTitle={key} contactValue={profile.contacts[key]}/>
+            })}
+            </div>
+        </div>
+    )
+};
+
+type ProfileDataFormPropsType = {
+    profile: ProfileType | null
+}
+const ProfileDataForm: React.FC<ProfileDataFormPropsType> = ({profile}) => {
+    return (
+        profile && <div>
+            <div>
+                <b>FullName:</b>{profile.fullName}
+            </div>
+            <div>
+                <b>LookingForAJob:</b>{profile.lookingForAJob ? 'YES' : 'No'}
+            </div>
+            {profile.lookingForAJob &&
+            <div>
+                <b>My professional skills:</b>{profile.lookingForAJobDescription}
+            </div>
+            }
+            <div>
+                <b>About me:</b>{profile.aboutMe ? 'YES' : 'No'}
+            </div>
+            <div>
+                <b>Contacts</b>: {(Object.keys(profile.contacts) as Array<keyof ContactsType>).map(key => {
+                return <Contact key={key} contactTitle={key} contactValue={profile.contacts[key]}/>
+            })}
+            </div>
+        </div>
+    )
+};
+
+
 type ContactPropsType = {
     contactTitle: string
     contactValue: any
 }
-
 const Contact: React.FC<ContactPropsType> = ({contactTitle, contactValue}) => {
-    return <div>
-        <b>{contactTitle}</b>: {contactValue}
-    </div>
+    return (
+        <div className={s.contact}>
+            <b>{contactTitle}</b>: {contactValue}
+        </div>
+    )
 
 }
