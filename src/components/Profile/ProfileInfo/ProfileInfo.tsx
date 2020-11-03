52d2139647512d7eb2f581f5d0bba938a0b91d15
Karter13 <karmih1975@mail.ru>
2020-11-03 17:01:12 +0300

    added profile data

--- src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import s from './ProfileInfo.module.css'
 import {Preloader} from '../../common/Preloader/Preloader';
-import {ProfileType} from '../../../redux/profile-reducer';
+import {ContactsType, ProfileType} from '../../../redux/profile-reducer';
 import {ProfileStatusWithHooks} from './ProfileStatusWithHooks';
 import userPhoto from '../../../assets/images/noavatar.png'
 
@@ -16,13 +16,14 @@ export const ProfileInfo: React.FC<ProfileInfoPropsType> = ({profile, status, up
     if (!profile) {
         return <Preloader/>
     }
-
+    //types for e?
+    // DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
     const onMainPhotoSelected = (e: any) => {
         if (e.target.files.length) {
             savePhoto(e.target.files[0])
         }
     };
-    // DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
+
     return (
         <div className={s.profile}>
             <div className={s.descriptionBlock}>
@@ -30,6 +31,29 @@ export const ProfileInfo: React.FC<ProfileInfoPropsType> = ({profile, status, up
                     <img src={profile.photos.large || userPhoto}/>
                 </div>
                 {isOwner && <input type={'file'} onChange={onMainPhotoSelected}/>}
+
+                <div>
+                    <div>
+                        <b>FullName:</b>{profile.fullName}
+                    </div>
+                    <div>
+                        <b>LookingForAJob:</b>{profile.lookingForAJob ? 'YES' : 'No'}
+                    </div>
+                    {profile.lookingForAJob &&
+                    <div>
+                        <b>My professional skills:</b>{profile.lookingForAJobDescription}
+                    </div>
+                    }
+                    <div>
+                        <b>About me:</b>{profile.aboutMe ? 'YES' : 'No'}
+                    </div>
+                    <div>
+                        <b>Contacts</b>: {Object.keys(profile.contacts).map(key => {
+                           return <Contact key={key} contactTitle = {key} contactValue = {'profile.contacts[key]' }/>
+                    })}
+                    </div>
+                </div>
+
                 <ProfileStatusWithHooks status={status}
                                         updateStatus={updateStatus}
                 />
@@ -37,3 +61,15 @@ export const ProfileInfo: React.FC<ProfileInfoPropsType> = ({profile, status, up
         </div>
     )
 };
+
+type ContactPropsType = {
+    contactTitle: string
+    contactValue: any
+}
+
+const Contact: React.FC<ContactPropsType> = ({contactTitle, contactValue}) => {
+    return <div>
+        <b>{contactTitle}</b>: {contactValue}
+    </div>
+
+}
